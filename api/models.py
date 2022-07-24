c5f7fab191a4b0e10a0a121393cef7ad3e785f7c
Jan Hofrichter <hofrrichter.jan1@gmail.com>
2022-07-24 17:41:42 +0200

    DB constraint fixed and using Django intern ID

--- api/models.py
+++ api/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class AttributeName(models.Model):
     DoesNotExist = None
-    id = models.IntegerField(primary_key=True)
     objects = None
     nazev = models.CharField(max_length=200, blank=True)
     kod = models.CharField(max_length=200, blank=True)
@@ -12,23 +11,20 @@ class AttributeName(models.Model):
 
 class AttributeValue(models.Model):
     DoesNotExist = None
-    id = models.IntegerField(primary_key=True)
     objects = None
     hodnota = models.CharField(max_length=200, blank=True)
 
 
 class Attribute(models.Model):
     DoesNotExist = None
-    id = models.IntegerField(primary_key=True)
     objects = None
-    hodnota_atributu_id = models.IntegerField(null=True)
-    nazev_atributu_id = models.IntegerField(blank=True)
+    hodnota_atributu_id = models.IntegerField(blank=True, null=True)
+    nazev_atributu_id = models.IntegerField(blank=True, null=True)
 
 
 class Product(models.Model):
     DoesNotExist = None
     objects = None
-    id = models.IntegerField(primary_key=True)
     nazev = models.CharField(max_length=200, blank=True)
     description = models.TextField(blank=True)
     cena = models.CharField(max_length=200, blank=True)
@@ -40,15 +36,13 @@ class Product(models.Model):
 class ProductAttributes(models.Model):
     DoesNotExist = None
     objects = None
-    id = models.IntegerField(primary_key=True)
-    attribute = models.IntegerField(blank=True)
-    product = models.IntegerField(blank=True)
+    attribute = models.IntegerField(blank=True, null=True)
+    product = models.IntegerField(blank=True, null=True)
 
 
 class Image(models.Model):
     DoesNotExist = None
     objects = None
-    id = models.IntegerField(primary_key=True)
     obrazek = models.URLField(blank=True)
     nazev = models.CharField(max_length=200, blank=True)
 
@@ -56,22 +50,18 @@ class Image(models.Model):
 class ProductImage(models.Model):
     DoesNotExist = None
     objects = None
-    id = models.IntegerField(primary_key=True)
-    product = models.IntegerField(blank=True)
-    obrazek_id = models.IntegerField(blank=True)
+    product = models.IntegerField(blank=True, null=True)
+    obrazek_id = models.IntegerField(blank=True, null=True)
     nazev = models.CharField(max_length=200, blank=True)
+
+
+#class Extend(models.Model):
+    #products_ids = models.IntegerField()
 
 
 class Catalog(models.Model):
     DoesNotExist = None
     objects = None
-    id = models.IntegerField(primary_key=True)
     nazev = models.CharField(max_length=200, blank=True)
     obrazek_id = models.IntegerField(blank=True, null=True)
-    #products_ids = models.JSONField(blank=True, null=True)
-    #attributes_ids = models.JSONField(blank=True, null=True)
-
-
-# class Extend(models.Model):
-#     products_ids = models.ManyToManyField(Catalog, blank=True, related_name="product_ids")
-#     attributes_ids = models.ManyToManyField(Catalog, blank=True, related_name="attributes_ids")
+    #products_ids = models.ForeignKey(Extend, on_delete=models.CASCADE, unique=True)
